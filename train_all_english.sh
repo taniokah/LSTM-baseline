94fed4cef06ddc6404b7627bb9dd5e50d641be74
Hiroki <taniokah@gmail.com>
2020-08-01 00:31:33 +0900

    Create train_all_english.sh

---
+++ train_all_english.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+PYTHON=python
+
+for task in "nugget" "quality"
+do
+    for language in "english"
+    do
+        echo "Training" $task $language;
+        $PYTHON train.py --task $task --language $language $@ || exit 1
+    done
+done
